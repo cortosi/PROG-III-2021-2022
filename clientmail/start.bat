6d1893b33ec63c69516098e17f13e4fb8b2740ac
vittorio.cortosi <73166428+cortosi@users.noreply.github.com>
2021-12-19 14:41:35 +0100

    Initial Commit
    .

---
+++ clientmail/start.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java --module-path C:\\Users\\vitto\\Desktop\\Prog\\Prog3\\ClientMail\\lib\\javafx-sdk-17.0.1\\lib --add-modules javafx.controls,javafx.fxml -jar out/artifacts/clientmail_jar/clientmail.jar
+PAUSE
